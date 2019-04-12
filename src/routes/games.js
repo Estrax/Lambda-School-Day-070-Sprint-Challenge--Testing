f15de72fc77d080949ccc3eb0eb81fd45e2fded6
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-12 13:06:40 -0400

    refactored routes to typical MVC

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,43 +1,12 @@
 const router = require('express').Router();
-const db = require('../models/games');
+const gamesController = require('../controllers/gamesController');
 
 router.route('/')
-    .get(async (req, res) => {
-        await db
-                .getAll()
-                .then(games => res.status(200).json(games));
-    })
-    .post(async (req, res) => {
-        if(!req.body.title || !req.body.genre) return res.status(422).json({ error: "You need to provide game title and genre!"});
-        await db
-                .insert(req.body)
-                .then(response => {
-                    response === 0 
-                        ? res.status(405).json({ error: 'Title must be unique!' })
-                        : res.status(201).json(response);
-                })
-                .catch(err => res.status(500).json({ error: "There was an error while saving this game to the database."}));
-    });
+    .get(gamesController.getAllGames)
+    .post(gamesController.addNewGame);
 
 router.route('/:id')
-    .get(async (req, res) => {
-        await db
-                .getById(Number(req.params.id))
-                .then(
-                    response => response === 0
-                        ? res.status(404).json({ error: "The game with the specified ID does not exist." })
-                        : res.status(200).json(response)
-                )
-                .catch(err => res.status(500).json({ error: "Game could not be retrieved."}));
-    })
-    .delete(async (req, res) => {
-        await db
-                .remove(Number(req.params.id))
-                .then(response => response === 0
-                    ? res.status(404).json({ error: "The game with the specified ID does not exist." })
-                    : res.status(200).json({ success: true })
-                )
-                .catch(err => res.status(500).json({ error: "Game could not be deleted." }));
-    });
+    .get(gamesController.getGame)
+    .delete(gamesController.deleteGame);
 
 module.exports = router;
